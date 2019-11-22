a7a9c4bbef5a30e17c0d17aa7ee53845c990525e
Zoltan Kelemen <zoltan.kelemen@saabgroup.com>
2019-11-22 13:03:38 +0100

    Remove security check - let the card handle the situation.

--- src/libopensc/card-iasecc.c
+++ src/libopensc/card-iasecc.c
@@ -1931,17 +1931,12 @@ iasecc_pin_is_verified(struct sc_card *card, struct sc_pin_cmd_data *pin_cmd_dat
 {
 	struct sc_context *ctx = card->ctx;
 	struct sc_pin_cmd_data pin_cmd;
-        struct sc_acl_entry acl = pin_cmd_data->pin1.acls[IASECC_ACLS_CHV_VERIFY];
-	int rv = SC_ERROR_SECURITY_STATUS_NOT_SATISFIED;
+	int rv;
 
 	LOG_FUNC_CALLED(ctx);
 
 	if (pin_cmd_data->pin_type != SC_AC_CHV)
 		LOG_TEST_RET(ctx, SC_ERROR_NOT_SUPPORTED, "PIN type is not supported for the verification");
-
-	sc_log(ctx, "Verify ACL(method:%X;ref:%X)", acl.method, acl.key_ref);
-	if (acl.method != SC_AC_SCB || acl.key_ref != IASECC_SCB_ALWAYS)
-		LOG_FUNC_RETURN(ctx, SC_ERROR_SECURITY_STATUS_NOT_SATISFIED);
 
 	pin_cmd = *pin_cmd_data;
 	pin_cmd.pin1.data = (unsigned char *)"";
